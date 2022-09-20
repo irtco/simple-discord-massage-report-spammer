f29fdc24846e25f522e0fd1a509985d15f6bdf3b
irtco <81117335+irtco@users.noreply.github.com>
2022-09-20 22:19:42 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+token=input("TOKEN ->")
+channel_ID=input("channel ID ->")
+Guild_ID=input("Server ID ->")
+Massage_ID=input("Massage ID ->")
+Reason=input("Reason (0-4)->")
+
+header={
+    "authorization": token,
+    "content-type": "application/json"
+}
+
+payload={
+    "channel_id": channel_ID,
+    "guild_id": Guild_ID,
+    "message_id": Massage_ID,
+    "reason": Reason
+}
+
+url='https://discord.com/api/v9/report'
+
+r=requests.post(url=url, headers=header, json=payload)
+
+print(r.content)
+
+i=1
+
+while i<5:
+    r=requests.post(url=url, headers=header, json=payload)
+    print(r.content)
